f32a58abab774041b340fc6e7e51bb50b73e71ef
Aparecida <aparecida@MacBook-Pro-de-Fix.local>
2020-08-04 13:13:41 -0300

    Primeiro commit do projeto de vue

---
+++ alurapic/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <ul v-for="foto of fotos">
+      <li><img :src="foto.url" :alt="foto.nome"></li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+  data(){
+
+    return{
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          nome: "Cachorro"
+        },
+        {
+          url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          nome: "Cachorro2"
+        }
+      ],
+    }
+
+  }
+
+}
+</script>
+
+<style>
+</style>
